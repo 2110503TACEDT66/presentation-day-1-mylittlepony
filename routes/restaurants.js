6728a02d070b3521cf7f797d7bbad0d6d36094ae
aiyapatv <6633288721@student.chula.ac.th>
2024-03-02 15:19:06 +0700

    fix restaurant bug

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -8,10 +8,9 @@ const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
-router.use('/:restaurantId/reservations/',reservationRouter);
+router.use('/:restaurantId/reservations/', reservationRouter);
 
 router.route('/').get(getRestaurants).post(protect,authorize('admin'), createRestaurant);
 router.route('/:id').get(getRestaurant).put(protect,authorize('admin'), updateRestaurant).delete(protect,authorize('admin'), deleteRestaurant);
 
-
-module.exports=router;
+module.exports = router;
